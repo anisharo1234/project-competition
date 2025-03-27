5003518ac5e83793403ac35c834ddad1339c5635
anisharo1234 <shindeanisha20@gmail.com>
2025-03-27 21:26:44 +0530

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 diabetes_model = pickle.load(open('diabetes_model.pkl', 'rb'))
 heart_model = pickle.load(open('heart_model.pkl', 'rb'))
 parkinsons_model = pickle.load(open('parkinson_model.pkl', 'rb'))
-breast_cancer_model = pickle.load(open('breast_cancer_model.pkl', 'rb'))
+breast_cancer_= pickle.load(open('breast_cancer_.pkl', 'rb'))
 
 # Streamlit page configuration
 st.set_page_config(page_title="Multiple Disease Prediction", layout="centered")
